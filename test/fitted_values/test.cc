bc3c3d5921b7e22215da48ee050d358ecf97c331
Ioannis Kingdom <jkingdom3@hotmail.com>
2021-06-20 20:18:51 +0100

    Test for fitted_values complete

--- test/fitted_values/test.cc
+++ test/fitted_values/test.cc
@@ -3,6 +3,8 @@
 
 #include <assert.h>
 
+#include <vector>
+
 
 int main() {
     // Test 1
@@ -11,8 +13,18 @@ int main() {
 
     Data<double> Y_bar = M.fitted_values();
 
+    std::vector<double> params(M.derive_transfer_vector_size());
+    std::vector<double> y_bar2 = M.fitted_values(params);
+    Data<double> Y_bar2;
+    Y_bar2.insert("E(log_mx)", y_bar2);
+
     Data<double> solution {data::file_to_double("solution.csv")};
 
     for (unsigned int k = 0; k < Y_bar.nrows(); k++) {
-        assert(abs(Y_bar["E(log_mx)"][k] - solution["E(log_mx)"][k]) < 1e-6);}
+        assert(abs(Y_bar["E(log_mx)"][k] - solution["E(log_mx)"][k]) < 1e-6);
+        assert(abs(Y_bar2["E(log_mx)"][k] - solution["E(log_mx)"][k]) < 1e-6);}
+    assert(Y_bar.nrows() == 10120);
+    assert(Y_bar.ncols() == 1);
+    assert(Y_bar2.nrows() == 10120);
+    assert(Y_bar2.ncols() == 1);
 }
